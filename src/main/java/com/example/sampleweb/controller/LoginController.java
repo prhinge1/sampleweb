980d3ddd631d48ecfc469ffe4e5417cb4313d016
Prasanna, Hinge <impdh@rediffmail.com>
2020-01-21 04:52:34 +0530

    New Project commited

---
+++ src/main/java/com/example/sampleweb/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.sampleweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String getLoginPage() {
+		System.out.println("HI");
+		return "/login";
+	}
+
+}
