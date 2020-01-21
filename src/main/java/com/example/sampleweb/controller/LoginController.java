70b48eb6914beeae7d06cead053d93adef5e50b0
Prasanna, Hinge <impdh@rediffmail.com>
2020-01-21 14:09:09 +0530

    Controller update

--- src/main/java/com/example/sampleweb/controller/LoginController.java
+++ src/main/java/com/example/sampleweb/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
+	@RequestMapping("/")
 	public String getLoginPage() {
 		System.out.println("HI");
 		return "/login";
